38c4d93d85493859c581ba591ea3bc7e18163505
jpereira <jpereira@geocom.com.uy>
2020-04-21 17:13:11 -0300

    Fix mysql connection url

--- src/main/java/MainJDBCFirstExample.java
+++ src/main/java/MainJDBCFirstExample.java
@@ -9,10 +9,10 @@ public class MainJDBCFirstExample {
     public static void main(String[] args) throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
 //        Class.forName("org.postgresql.Driver");
-//        Connection con = DriverManager.getConnection(
-//                "jdbc:mysql://localhost:3306/test2020","root","geocom");
         Connection con = DriverManager.getConnection(
-                "jdbc:postgresql://localhost/test2","postgres","geocom");
+                "jdbc:mysql://localhost:3306/test2020","root","geocom");
+//        Connection con = DriverManager.getConnection(
+//                "jdbc:postgresql://localhost/test2","postgres","geocom");
         Statement stmt = con.createStatement();
         ResultSet rs = stmt.executeQuery("select * from employee");
         while(rs.next())
