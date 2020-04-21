35a78bd932756ead78ce7e7b8b3ea30fdd74c715
jpereira <jpereira@geocom.com.uy>
2020-04-21 16:56:37 -0300

    First commit

---
+++ src/main/java/MainJDBCFirstExample.java
@@ -0,0 +1,23 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MainJDBCFirstExample {
+
+    public static void main(String[] args) throws Exception {
+//        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("org.postgresql.Driver");
+//        Connection con = DriverManager.getConnection(
+//                "jdbc:mysql://localhost:3306/test2020","root","geocom");
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://localhost/test2","postgres","geocom");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from employee");
+        while(rs.next())
+            System.out.println(rs.getString("ssn")+"  "+rs.getString("name")+"  "+rs.getInt("age"));
+        con.close();
+    }
+
+}
